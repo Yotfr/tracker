bc98ff23887cc1f1dac1324c64f109fb7c833a41
Yotfr <yotfr73@gmail.com>
2023-08-24 12:24:52 +0400

    added deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,16 +4,17 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.google.devtools.ksp")
+    //id("kotlin-kapt")
 }
 
 android {
     namespace = "ru.yotfr.tracker"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "ru.yotfr.tracker"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -43,7 +44,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.5.2"
     }
     packaging {
         resources {
@@ -55,11 +56,46 @@ android {
 dependencies {
 
     val roomVersion = "2.5.2"
+    val viewModelKTXVersion = "2.6.1"
+    val coroutinesVersion = "1.7.2"
+    val navVersion = "2.7.1"
+    val glideVersion = "4.14.2"
+    val glideComposeVersion = "1.0.0-alpha.5"
+    val googleLocationServicesVersion = "21.0.1"
+    val googleMapsServicesVersion = "18.1.0"
+    val daggerVersion = "2.47"
+    val timberVersion = "5.0.1"
+
 
     // Room
     implementation("androidx.room:room-runtime:$roomVersion")
-    annotationProcessor("androidx.room:room-compiler:$roomVersion")
+    ksp("androidx.room:room-compiler:$roomVersion")
 
+    // KTX
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$viewModelKTXVersion")
+    implementation("androidx.room:room-ktx:$roomVersion")
+
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+
+    // Navigation
+    implementation("androidx.navigation:navigation-compose:$navVersion")
+
+    // Glide
+    implementation("com.github.bumptech.glide:compose:$glideComposeVersion")
+    ksp("com.github.bumptech.glide:ksp:$glideVersion")
+
+    // Google maps and location services
+    implementation("com.google.android.gms:play-services-location:$googleLocationServicesVersion")
+    implementation("com.google.android.gms:play-services-maps:$googleMapsServicesVersion")
+
+    // Dagger
+    implementation("com.google.dagger:dagger:$daggerVersion")
+    ksp("com.google.dagger:dagger-compiler:$daggerVersion")
+
+    // Timber
+    implementation("com.jakewharton.timber:timber:$timberVersion")
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
